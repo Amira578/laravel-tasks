e478fc273c18ba4d7453c1d655455844bd70eac3
Amira <es-amira.khalid2018@alexu.edu.eg>
2021-01-26 16:08:18 +0200

    after edit

--- laravel/project1/app/Http/Controllers/PageController.php
+++ laravel/project1/app/Http/Controllers/PageController.php
@@ -4,29 +4,30 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Support\Facades\DB;
-use App\Models\project;
+use App\Models\Project;
 
 class PageController extends Controller
-{  public function home(){
-    project::query()->create([
+{  public function home()
+  {
+    Project::query()->create([
         'title'=> 'project title 1',
         'description'=>'project description one'
     ]);
-    project::query()->create([
+    Project::query()->create([
         'title'=> 'project title 2',
         'description'=>'project description two'
     ]);
-    project::query()->create([
+    Project::query()->create([
         'title'=> 'project title 3',
         'description'=>'project description three'
     ]);
 
-    project::where('title','project title 1')->update(['title'=>'updated title']);
-    project::where('title','project title 2')->delete();
+    Project::where('title','project title 1')->update(['title'=>'updated title']);
+    Project::where('title','project title 2')->delete();
     //$project= DB::table('projects')->get();
 
     //print_r($project);
-    dd(project::all());
+    dd(Project::all());
 
 
 
--- laravel/project1/app/Models/Project.php
+++ laravel/project1/app/Models/Project.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class project extends Model
+class Project extends Model
 {
     use HasFactory,SoftDeletes;
 
